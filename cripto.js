418b4a1f01ff673a322ca142f4ee3ce15e5cc203
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-04-30 20:50:53 -0300

    responsivo

--- cripto.js
+++ cripto.js
@@ -1,26 +1,48 @@
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let cripto = []
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-// console.log(cripto.join('')) //converte o array em string
+console.log(alphabet.length)
 
-const criptografy = (userInput, keyInputUser) => {
-    cripto = []
+function criptografy(userInput, keyInputUser) {
+  let cripto = '';
 
-    Array.from(userInput).map(caractere => {
-        
-        if(caractere === ' '){
-            cripto.push(' ')
-        }
+  for (let i = 0; i < userInput.length; i++) {
+    const char = userInput[i].toLowerCase();
+    const index = alphabet.indexOf(char);
 
-        for (let i = 0; i <= alphabet.length; i++) {
+    if (index === -1) {
+      cripto += char;
+    } else {
+      const newIndex = (index + Number(keyInputUser)) % alphabet.length;
+      cripto += alphabet[newIndex];
+    }
+  }
 
-            if (caractere.toLowerCase() === alphabet[i]) {
-                cripto.push(alphabet[i + Number(keyInputUser)])
-            }
-        }
-    })
-
-    resultFrame.innerHTML = ''
-    resultFrame.insertAdjacentText('afterbegin', cripto.join(''))
-    console.log(cripto)
+  resultFrame.textContent = cripto;
 }
+
+// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// let cripto = []
+
+// // console.log(cripto.join('')) //converte o array em string
+
+// const criptografy = (userInput, keyInputUser) => {
+//     cripto = []
+
+//     Array.from(userInput).map(caractere => {
+        
+//         if(caractere === ' '){
+//             cripto.push(' ')
+//         }
+
+//         for (let i = 0; i <= alphabet.length; i++) {
+
+//             if (caractere.toLowerCase() === alphabet[i]) {
+//                 cripto.push(alphabet[i + Number(keyInputUser)])
+//             }
+//         }
+//     })
+
+//     resultFrame.innerHTML = ''
+//     resultFrame.insertAdjacentText('afterbegin', cripto.join(''))
+//     console.log(cripto)
+// }
