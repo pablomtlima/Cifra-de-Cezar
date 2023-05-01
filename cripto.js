49c6247861c1a235a3f44153a93591e4cb721420
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-04-30 21:11:49 -0300

    comentario do codigo inicial

--- cripto.js
+++ cripto.js
@@ -20,6 +20,9 @@ function criptografy(userInput, keyInputUser) {
   resultFrame.textContent = cripto;
 }
 
+
+// codigo inicial sem refatoração
+
 // const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // let cripto = []
 
