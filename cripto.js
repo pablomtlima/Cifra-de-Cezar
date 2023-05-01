5061a812bc832276d611ac71d2e8f11fbe546b50
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-05-01 18:42:32 -0300

    melhor sem espaçõs

--- cripto.js
+++ cripto.js
@@ -3,22 +3,13 @@
 function criptografy(userInput, keyInputUser) {
   let encryptedData = ''
 
-  for( let index in userInput) {
+  for (let index in userInput) {
 
-    console.log(userInput[index])
-
-    
     const currentCharCode = userInput.charCodeAt(index)
-
-    if(userInput[index] === ' '){
-      console.log('é space')
-      encryptedData += ' '
-    } else {
-      const newCharCode = currentCharCode + keyInputUser
-      encryptedData += String.fromCharCode(newCharCode)
-    }
+    const newCharCode = currentCharCode + keyInputUser
+    encryptedData += String.fromCharCode(newCharCode)
   }
 
-   return encryptedData
+  return encryptedData
 }
 
