157f2b76c1e955181c1773b41515fbb4d87e838b
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-05-01 17:15:29 -0300

    ok

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const input = document.querySelector('input')
 const submit = document.querySelector('#submit')
 const resultFrame = document.querySelector('.result')
 const keyInput = document.querySelector('#key')
-const encrypt = document.querySelector('.encrypt')
-const decrypt = document.querySelector('.decrypt')
+const encrypt = document.querySelector('#encrypt')
+const decrypt = document.querySelector('#decrypt')
+const encryptFrame = document.querySelector('.encryptFrame')
+const decryptFrame = document.querySelector('.decryptFrame')
 
+console.log(encrypt.getAttributeNames())
+console.log(decrypt.getAttributeNames())
+
+encrypt.addEventListener('click', ()=>{
+    decryptFrame.classList.add('hidden')
+    encryptFrame.classList.remove('hidden')
+})
+
+decrypt.addEventListener('click', ()=>{
+
+    encryptFrame.classList.add('hidden')
+    decryptFrame.classList.remove('hidden')
+})
 
 let keyInputUser = 0
 
@@ -15,7 +30,7 @@ function insertResult(event) {
         event.preventDefault();
 
         const resultEncryptedData = criptografy(userInput, keyInputUser)
-        
+
         resultFrame.textContent = resultEncryptedData
     })
 }
